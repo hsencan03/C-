a79228a658dbbf7aaad730b797b46b67057b5fd6
Huseyin Sencan <hsencan03@gmail.com>
2018-04-10 19:14:48 +0300

    Snake
    C#

---
+++ SnakeProject/SnakeProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Snake game = new Snake(25,100);
+            game.printMap();
+
+            Console.ReadKey();
+        }
+    }
+}
