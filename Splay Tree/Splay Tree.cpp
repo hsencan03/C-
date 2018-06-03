cbe3df2bebeef3aff21c18cdfbefd6e789a56104
Hüseyin Şencan <34361572+hsencan03@users.noreply.github.com>
2018-06-03 06:11:09 +0300

    Add fixed

--- Splay Tree/Splay Tree.cpp
+++ Splay Tree/Splay Tree.cpp
@@ -1,110 +1,119 @@
 //Splay Tree
+#include "stdafx.h"
 #include <iostream>
 
-struct Node{
+struct Node {
 	int key;
-	Node *left,*right;
-	
-	Node(int key_):key(key_){
-		left=right=nullptr;
+	Node *left, *right;
+
+	Node(int key_) :key(key_) {
+		left = right = nullptr;
 	}
 };
 //Tree Class
-class Tree{
+class Tree {
 public:
-	static Node *add(Node *root,int key){
-		if(root==nullptr)	
-			return new Node(key);			
-		if(root->key > key)
-			root->left=add(root->left,key);
-		else if(root->key < key)
-			root->right=add(root->right,key);
+	static Node *add(Node *root, int key) {
+		if (root == nullptr)
+			return new Node(key);
+		if (root->key > key) 
+		{
+			root->left = add(root->left, key);
+			return root;
+		}
+		else if (root->key < key) 
+		{
+			root->right = add(root->right, key);
+			return root;
+		}
 		else
 			return root;
 	}
-	static bool search(Node *&root,int key_){
-		root=splay(root,key_);
-		return (root->key==key_ ? true : false);
+	static bool search(Node *&root, int key_) {
+		root = splay(root, key_);
+		return (root->key == key_ ? true : false);
 	}
 private:
-	static Node *rightRotate(Node *root){
-		Node *temp=root->left;
-		root->left=temp->right;
-		temp->right=root;
-		
+	static Node *rightRotate(Node *root) {
+		Node *temp = root->left;
+		root->left = temp->right;
+		temp->right = root;
+
 		return temp;
 	}
 	static Node *leftRotate(Node *root)
 	{
-		Node *temp=root->right;
-		root->right=temp->left;
-		temp->left=root;
-		
+		Node *temp = root->right;
+		root->right = temp->left;
+		temp->left = root;
+
 		return temp;
 	}
 	//Splay 
-	static Node *splay(Node *root,int key)
+	static Node *splay(Node *&root, int key)
 	{
-		if(root==nullptr || root->key==key)
+		if (root == nullptr || root->key == key)
 			return root;
-		
-		if(root->key>key)
+
+		if (root->key>key)
 		{
-			if(root->left==nullptr)
+			if (root->left == nullptr)
 				return root;
-			
+
 			//Zig Zig (left-left)
-			if(root->left->key>key)
+			if (root->left->key>key)
 			{
-				root->left->left=splay(root->left->left,key);
-				
-				root=rightRotate(root);
+				root->left->left = splay(root->left->left, key);
+
+				root = rightRotate(root);
 			}
 			//Zig Zag(left-right)
-			else if(root->left->key<key)
+			else if (root->left->key<key)
 			{
-				root->left->right=splay(root->left->right,key);
-				
-				if(root->left->right!=nullptr)
-					root->left=leftRotate(root->left);
+				root->left->right = splay(root->left->right, key);
+
+				if (root->left->right != nullptr)
+					root->left = leftRotate(root->left);
 			}
-			return (root->left==nullptr) ? root : rightRotate(root);
+			return (root->left == nullptr) ? root : rightRotate(root);
 		}
-		else if(root->key<key)
+		else if (root->key<key)
 		{
-			if(root->right==nullptr)
+			if (root->right == nullptr)
 				return root;
 			//Zag-Zag (right-right)
-			if(root->right->key<key)
+			if (root->right->key<key)
 			{
-				root->right->right=splay(root->right->right,key);
-				
-				root=leftRotate(root);
+				root->right->right = splay(root->right->right, key);
+
+				root = leftRotate(root);
 			}
 			//Zag-Zig(Right Left)
-			else if(root->right->key>key)
+			else if (root->right->key>key)
 			{
-				root->right->left=splay(root->right->left,key);
-				
-				if(root->right->left!=nullptr)
-					root->right=rightRotate(root->right);
+				root->right->left = splay(root->right->left, key);
+
+				if (root->right->left != nullptr)
+					root->right = rightRotate(root->right);
 			}
-			return (root->right==nullptr) ? root : leftRotate(root);
+			return (root->right == nullptr) ? root : leftRotate(root);
 		}
 	}
 };
 
 //Test
-int main(){
-	Node *root=nullptr;
-	
-	root=Tree::add(root,100);
-	root=Tree::add(root,200);
-	root=Tree::add(root,50);
-	root=Tree::add(root,40);
-	
-	std::cout<<Tree::search(root,50)<<std::endl;
-	
-	std::cout<<root->key;
+int main() {
+	Node *root = nullptr;
+
+	root=Tree::add(root, 100);
+	root=Tree::add(root, 200);
+	root=Tree::add(root, 50);
+	root=Tree::add(root, 40);
+
+	std::cout << root->key << std::endl;
+
+	std::cout << Tree::search(root, 48787870) << std::endl;
+
+	std::cout << root->key;
 
 }
